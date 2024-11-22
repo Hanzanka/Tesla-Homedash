8b9a9e52d38913bd0006eb8d81bdaf4367b692c0
Hanzanka <ville.einio@gmail.com>
2024-11-22 03:06:30 +0200

    Add: Crosshair now locks into the closest x value found from the data. Also add textboxes to display the values from mouse pos

--- src/ui/plot/dataplot.py
+++ src/ui/plot/dataplot.py
@@ -1,3 +1,4 @@
+# Needed to be imported for other modules to be able to access some dlls
 if __name__ == "__main__":
     from PySide6.QtCore import Qt
 
@@ -9,6 +10,12 @@ import numpy as np
 
 
 class DataPlot(QFrame):
+    '''
+    Used to make interactive plots from data
+
+    Args:
+        
+    '''
     def __init__(self) -> None:
         super().__init__()
 
@@ -19,7 +26,7 @@ class DataPlot(QFrame):
         self.__graph.viewport().setMouseTracking(True)
 
         self.__plot = self.__graph.addPlot()
-        self.__plot.setRange(xRange=(0, 10), yRange=(1, -1))
+        self.__plot.setRange(xRange=(-5, 5), yRange=(2, -2))
         self.__plot.setMenuEnabled(False)
         self.__plot.setMouseEnabled(x=False, y=False)
         self.__plot.hideButtons()
@@ -31,23 +38,58 @@ class DataPlot(QFrame):
             self.__crosshair,
         )
 
+        self.__x_value = pg.TextItem(
+            text="",
+            color="black",
+            anchor=(0.5, 0.5),
+            border=pg.mkPen("white"),
+            fill=pg.mkBrush("white"),
+        )
+        self.__x_value.setZValue(2)
+        self.__plot.addItem(self.__x_value)
+
+        self.__y_value = pg.TextItem(
+            text="",
+            color="black",
+            anchor=(0.5, 0.5),
+            border=pg.mkPen("white"),
+            fill=pg.mkBrush("white"),
+        )
+        self.__y_value.setZValue(2)
+        self.__plot.addItem(self.__y_value)
+
         grad = QLinearGradient(0, 0, 0, 6)
         grad.setColorAt(0.0, pg.mkColor("#000000"))
         grad.setColorAt(0.5, pg.mkColor("orange"))
         brush = QBrush(grad)
 
         # Testing:
-        x = np.linspace(0, 10, 500)
-        y = np.cos(x)
-        self.__plot.plot(x, y, pen=pg.mkPen("orange"), brush=brush, fillLevel=0)
+        self.__x_data = np.linspace(start=-5, stop=5, num=1000)
+        self.__y_data = np.cos(5 * self.__x_data)
+        self.__plot.plot(
+            self.__x_data,
+            self.__y_data,
+            pen=pg.mkPen("orange"),
+            brush=brush,
+            fillLevel=0,
+        )
 
         self.__layout.addWidget(self.__graph)
         self.setLayout(self.__layout)
 
     @Slot(tuple)
     def __on_mouse_moved(self, pos):
-        mousePoint = self.__plot.vb.mapSceneToView(pos)
-        self.__crosshair.setPos(mousePoint)
+        mouse_point = self.__plot.vb.mapSceneToView(pos)
+        closest_index = np.abs(self.__x_data - mouse_point.x()).argmin()
+
+        x_value = self.__x_data[closest_index]
+        y_value = self.__y_data[closest_index]
+
+        self.__x_value.setText(str(x_value))
+        self.__y_value.setText(str(y_value))
+        self.__crosshair.setPos(x_value)
+        self.__x_value.setPos(x_value, -2)
+        self.__y_value.setPos(x_value, y_value)
 
 
 if __name__ == "__main__":
