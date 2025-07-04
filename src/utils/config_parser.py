125711739ee85cbaa43ac094594c2499cf59bc29
Ville Einiö <ville.einio@tuni.fi>
2025-07-04 17:43:08 +0300

    Create: Config parser base class

---
+++ src/utils/config_parser.py
@@ -0,0 +1,12 @@
+import json
+import os
+from dotenv import load_dotenv
+
+
+class ConfigUtils:
+    @staticmethod
+    def get_config() -> dict:
+        load_dotenv()
+        config_path = os.getenv("CONFIG_PATH")
+        with open(config_path, 'r') as file:
+            return json.load(file)
