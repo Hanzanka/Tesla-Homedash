b4949c1976342a3811327fe04a81c1e6f06c8a37
Hanzanka <ville.einio@gmail.com>
2025-09-30 18:17:10 +0300

    Add: functions to recieve dataproperties, as classes and json

--- src/tesla_service/vehicle.py
+++ src/tesla_service/vehicle.py
@@ -1,14 +1,19 @@
 import asyncio
 from ..utils.config_parser import ConfigUtils
 from .vehicle_data_property import VehicleDataProperty
+from ..influxdb_service.influxdb_handler import InfluxDBHandler
+from .server import TeslaDataServer
+from json import dumps
 
 
 class Vehicle:
-    def __init__(self, vin: str):
+    def __init__(self, vin: str, influx_db_handler: InfluxDBHandler, server: TeslaDataServer):
         self.__vin = vin
         self.__asleep = False
         self.__data = {}
         self.__async_lock = asyncio.Lock()
+        self.__influx_handler = influx_db_handler
+        self.__server = server
         self.__load_data_properties()
 
     def __load_data_properties(self) -> None:
@@ -17,9 +22,10 @@ class Vehicle:
             self.__data[data_property_id] = VehicleDataProperty(
                 data_id=data_property_id,
                 category=config["category"],
+                vehicle=self,
                 unit=config["unit"],
                 formula=config["formula"],
-                log=config["log"]
+                log=config["log"],
             )
 
     def on_telemetry_event(self, data) -> None:
@@ -28,31 +34,66 @@ class Vehicle:
     async def __update(self, data) -> None:
         if data["vin"] != self.__vin:
             return
-        
+
         if "timestamp" not in data:
             return
         timestamp = data["timestamp"]
-        
+
         if "data" not in data:
             if "state" in data:
                 online = data["state"] == "online"
-                await self.__data["VehicleOnline"].update(value=online, timestamp=timestamp)
+                await self.__data["VehicleOnline"].update(
+                    value=online, timestamp=timestamp
+                )
             return
-        
+
         async with self.__async_lock:
             vehicle_data = data["data"]
-            tasks = []
+            update_tasks = []
+            log = []
             for data_property_id, value in vehicle_data.items():
                 if data_property_id not in self.__data:
                     continue
+                data_property = self.__data[data_property_id]
                 task = asyncio.create_task(
-                    coro=self.__data[data_property_id].update(
-                        value=value, timestamp=timestamp
-                    )
+                    coro=data_property.update(value=value, timestamp=timestamp)
                 )
-                tasks.append(task)
-            await asyncio.gather(*tasks)
+                update_tasks.append(task)
+                if await data_property.get_logging():
+                    log.append(data_property)
+            await asyncio.gather(*update_tasks)
+
+            logging_tasks = []
+            for data_property in log:
+                task = asyncio.create_task(coro=data_property.get_influxdb_point())
+                logging_tasks.append(task)
+            log_points = await asyncio.gather(*logging_tasks)
+
+    async def get_vin(self) -> str:
+        async with self.__async_lock:
+            return self.__vin
 
     @property
     def vin(self) -> str:
         return self.__vin
+    
+    async def get_data_property(self, id: str) -> VehicleDataProperty:
+        async with self.__async_lock:
+            return self.__data[id]
+    
+    async def get_data_properties(self, ids: list) -> list:
+        data_properties = []
+        for id in ids:
+            data_properties.append(await self.get_data_property(id))
+        return data_properties
+
+    async def get_data_property_as_json(self, id: str) -> str:
+        async with self.__async_lock:
+            return self.__data[id].as_json()
+    
+    async def get_data_properties_as_json(self, ids: list) -> str:
+        async with self.__async_lock:
+            data = []
+            for id in ids:
+                data.append(await self.__data[id].get_as_dict())
+            return dumps(data)
