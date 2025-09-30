58047fdb61d9de5acf6fe672c96e7863b5ebc2fc
Hanzanka <ville.einio@gmail.com>
2025-09-30 18:17:59 +0300

    Add: receive data from dataproperty as json or dict

--- src/tesla_service/vehicle_data_property.py
+++ src/tesla_service/vehicle_data_property.py
@@ -1,5 +1,9 @@
 import asyncio
 from sympy import symbols, sympify
+from influxdb_client import Point, WritePrecision
+import datetime
+from datetime import timezone
+from json import dumps
 
 
 class VehicleDataProperty:
@@ -7,6 +11,7 @@ class VehicleDataProperty:
         self,
         data_id: str,
         category: str,
+        vehicle,
         unit: str = None,
         formula: str = None,
         log: bool = False,
@@ -22,32 +27,102 @@ class VehicleDataProperty:
             self.__sympy_expr = sympify(self.__formula)
         self.__log = log
         self.__async_lock = asyncio.Lock()
+        self.__vehicle = vehicle
+        self.__value_type = None
 
     async def update(self, value, timestamp) -> None:
+        if value is None:
+            return
+        if self.__value_type is None:
+            if isinstance(value, bool):
+                self.__value_type = "value_bool"
+            elif isinstance(value, int) or isinstance(value, float):
+                self.__value_type = "value_float"
+            elif isinstance(value, str):
+                self.__value_type = "value_string"
+            elif isinstance(value, dict):
+                self.__value_type = "value_dict"
         async with self.__async_lock:
             if self.__formula is not None:
-                self.__value = round(
-                    self.__sympy_expr.subs(self.__sympy_x, value).evalf(), 3
+                self.__value = float(
+                    self.__sympy_expr.subs(self.__sympy_x, value).evalf()
                 )
             else:
-                self.__value = value
+                if self.__value_type == "value_float":
+                    self.__value = float(value)
+                else:
+                    self.__value = value
             self.__timestamp = timestamp
-            print(
-                f"Dataproperty: {self.__id} updated | value: {self.__value} {f"{self.__unit} " if self.__unit is not None else ""}| timestamp: {self.__timestamp}"
-            )
 
     async def get_value(self):
-        with self.__async_lock:
+        async with self.__async_lock:
             return self.__value
 
     async def get_id(self):
-        with self.__async_lock:
+        async with self.__async_lock:
             return self.__id
 
     async def get_category(self):
-        with self.__async_lock:
+        async with self.__async_lock:
             return self.__category
 
     async def get_unit(self):
-        with self.__async_lock:
+        async with self.__async_lock:
             return self.__unit
+
+    async def get_logging(self):
+        async with self.__async_lock:
+            return self.__log
+
+    async def get_value_type(self):
+        async with self.__async_lock:
+            return self.__value_type
+
+    async def get_influxdb_point(self) -> Point:
+        async with self.__async_lock:
+            if self.__value is None or self.__timestamp is None:
+                return
+            try:
+                point = (
+                    Point("tesla_data")
+                    .tag("vin", self.__vehicle.vin)
+                    .tag("category", self.__category)
+                    .tag("id", self.__id)
+                    .time(
+                        datetime.datetime.fromtimestamp(self.__timestamp / 1000, tz=timezone.utc),
+                        WritePrecision.MS,
+                    )
+                )
+                if self.__value_type == "value_dict":
+                    for key, value in self.__value.items():
+                        point = point.field(key, value)
+                else:
+                    point = point.field(self.__value_type, self.__value)
+                return point
+            except Exception as e:
+                print(e)
+                return
+    
+    async def get_as_json(self) -> str:
+        async with self.__async_lock:
+            data = {
+                "id": self.__id,
+                "category": self.__category,
+                "unit": self.__unit,
+                "value": self.__value,
+                "value_type": self.__value_type,
+                "timestamp": self.__timestamp
+            }
+            return dumps(data)
+
+    async def get_as_dict(self) -> dict:
+        async with self.__async_lock:
+            data = {
+                "id": self.__id,
+                "category": self.__category,
+                "unit": self.__unit,
+                "value": self.__value,
+                "value_type": self.__value_type,
+                "timestamp": self.__timestamp
+            }
+            return data
