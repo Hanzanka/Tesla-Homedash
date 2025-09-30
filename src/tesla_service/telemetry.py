14f6529df6ce801041a9c074ef74601306b40d2d
Hanzanka <ville.einio@gmail.com>
2025-09-30 18:16:09 +0300

    Remove: telemetry test

--- src/tesla_service/telemetry.py
+++ src/tesla_service/telemetry.py
@@ -2,7 +2,7 @@ import asyncio
 from aiohttp import ClientSession
 from teslemetry_stream import TeslemetryStream
 from .vehicle import Vehicle
-
+from .server import TeslaDataServer
 
 class TelemetryHandler:
     def __init__(self, access_token: str, server: str, vehicle: Vehicle) -> None:
@@ -17,12 +17,12 @@ class TelemetryHandler:
         async with ClientSession() as session:
             self.__stream = TeslemetryStream(
                 access_token=self.__access_token,
-                vin=self.__vehicle.vin,
+                vin=await self.__vehicle.get_vin(),
                 server=self.__server,
                 session=session,
-                parse_timestamp=True
+                parse_timestamp=True,
             )
-            
+
             await self.__stream.connect()
 
             self.__close_stream = self.__stream.async_add_listener(
@@ -38,15 +38,3 @@ class TelemetryHandler:
         else:
             return
         self.__close_event.set()
-
-
-if __name__ == "__main__":
-    from dotenv import load_dotenv
-    import os
-
-    load_dotenv()
-    vh = Vehicle(vin=os.getenv("VIN"))
-    th = TelemetryHandler(
-        access_token=os.getenv("API_KEY"), server="eu.teslemetry.com", vehicle=vh
-    )
-    asyncio.run(th.start())
